00a2180198286cc820efa073d0d2be16984705c9
zuu <zxgamingh@gmail.com>
2025-03-31 11:45:46 +0200

    Now async compatible for login and logout redirects. Server is now able to get all information before site load

--- src/app/components/userManagement/login.tsx
+++ src/app/components/userManagement/login.tsx
@@ -2,42 +2,56 @@
 import RegisterField from "./registerFields";
 import Button from "../button";
 import { errors } from "../../lib/Errors";
+import { useRouter } from "next/navigation";
 
-function getLoginInputs(){
-  const nameInput = document.getElementById("usernameField") as HTMLInputElement;
-  const passwordInput = document.getElementById("passwordField") as HTMLInputElement;
-
-  if (!nameInput || !passwordInput){
-    throw new Error(errors.input_nan);
-  }
-  const userLogin  = {
-    username : nameInput.value,
-    password : passwordInput.value,
-  }
-
-  console.log(userLogin);
-
-  fetch("/api/login", 
-  {method : "POST", 
-  headers : {
-    "Accept" : "application/json",
-    "Content-Type" : "application/json"
-  },
-  body : JSON.stringify(userLogin)
-  })
-  .then(
-    (value : Response) => {value.status}
-  );
-
-}
 
 export default function loginInputs(){
+  const router = useRouter();
+
+  async function getLoginInputs(){
+    const nameInput = document.getElementById("usernameField") as HTMLInputElement;
+    const passwordInput = document.getElementById("passwordField") as HTMLInputElement;
+  
+    if (!nameInput || !passwordInput){
+      throw new Error(errors.input_nan);
+    }
+    const userLogin  = {
+      username : nameInput.value,
+      password : passwordInput.value,
+    }
+  
+    console.log(userLogin);
+  
+    try {
+      const response = await fetch(
+        "/api/login", 
+        {method : "POST", 
+        headers : {
+          "Accept" : "application/json",
+          "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(userLogin)
+        });
+  
+        if (response.ok) {
+          router.push("/");
+          window.location.reload();
+        } else {
+          console.error("Login failed", await response.json());
+        }
+  
+    } catch(err) {
+      console.error("Error during login", err);
+    }
+  }
+
   return (
     <article>
       <RegisterField idName="usernameField"/>
       <RegisterField idName="passwordField"/>
-      
+
       <Button buttonFunc={getLoginInputs} buttonText={"Login"}/>
+
     </article>
   );
 }
--- src/app/components/userManagement/logout.tsx
+++ src/app/components/userManagement/logout.tsx
@@ -1,10 +1,18 @@
 "use client";
 import Button from "../button";
 
-function userLogout(){
-  fetch("/api/logout", {method : "POST"});
-  window.location.reload();
-  console.log("user sucessfully logged out!");
+async function userLogout(){
+  try {
+    const response = await fetch("/api/logout", {method : "POST"});
+    if (response.ok) {
+      window.location.reload();
+      console.log("user sucessfully logged out!");
+    } else {
+      console.error("Log out error");
+    }
+  } catch(err) {
+    console.error("Error while logging out", err);
+  }
 }
 
 export default function Logout(){
