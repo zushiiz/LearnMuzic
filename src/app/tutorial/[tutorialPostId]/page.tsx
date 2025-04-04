394df157636d5912b3842497707b1ca126fdaf0c
zuu <zxgamingh@gmail.com>
2025-04-04 15:06:50 +0200

    imported songDisplay and tutorialDisplay, fetch works

--- src/app/tutorial/[tutorialPostId]/page.tsx
+++ src/app/tutorial/[tutorialPostId]/page.tsx
@@ -2,6 +2,8 @@
 import { useEffect, useState } from "react"
 import { Tutorial, Song } from "../../lib/dbTypes"
 import { httpErrors } from "../../lib/httpErrors";
+import SongDisplay from "../../components/postManagement/songDisplay";
+import TutorialDisplay from "../../components/postManagement/tutorialDisplay";
 
 interface TutorialPageProps {
   params : Promise<{
@@ -11,31 +13,59 @@ interface TutorialPageProps {
 
 
 export default function TutorialPage( {params} : TutorialPageProps ){
-  const [tutorialData, setTutorialData] = useState<Tutorial[]>([]); 
-  const [songData, setSongData] = useState<Song[]>([]);
+  const [tutorialData, setTutorialData] = useState<Tutorial>(); 
+  const [songData, setSongData] = useState<Song>();
   const [loading, setLoading] = useState<boolean>(true);
-
-  
 
   useEffect(() => {
     const fetchTutorial = async () => {
-      
+
       try {
         const postId = (await params).tutorialPostId;
         const response = await fetch(`/api/tutorial?tutorialPostId=${postId}`);
+        const data  = await response.json();
+
+        setSongData(data.songInfo);
+        setTutorialData(data.tutorialInfo);
+      
       } catch (err) {
         return httpErrors.badRequest;
+      } finally {
+        setLoading(false);
       }
       
-
     }
     fetchTutorial();
   }, []);
 
   return (
-    <div>
-      a
-    </div>
+    <section>
+      {songData ? (
+        <SongDisplay
+        imagePath={songData.imagePath}
+        title={songData.title}
+        artistId={songData.artistId}
+        genre={songData.genre}
+        releaseYear={songData.releaseYear}
+        />
+      ) : (
+        null
+      )
+      }
+      {tutorialData ? (
+        <TutorialDisplay
+        videoEmbed={tutorialData?.videoEmbed}
+        author={tutorialData?.author}
+        instrument={tutorialData?.instrument}
+        difficulty={tutorialData?.difficulty}
+        />
+
+      ) : (
+        null
+      )
+    }
+
+    </section>
   );
 
 }
