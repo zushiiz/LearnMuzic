9f6591e7d6fc116ccbf888085edf7d878f84d7cb
zuu <zxgamingh@gmail.com>
2025-04-04 15:02:44 +0200

    fixed attribute casing

--- src/app/components/videoEmbedPlayer.tsx
+++ src/app/components/videoEmbedPlayer.tsx
@@ -7,12 +7,12 @@ export default function VideoPlayer({videoEmbedId} : VideoProps){
     <iframe 
       width="560" 
       height="315" 
-      src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=CYHkd68n9T4n0N-q"
+      src={`https://www.youtube.com/embed/${videoEmbedId}`}
       title="YouTube video player" 
-      frameborder="0" 
+      frameBorder="0" 
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-      referrerpolicy="strict-origin-when-cross-origin" 
-      allowfullscreen>
+      referrerPolicy="strict-origin-when-cross-origin" 
+      allowFullScreen>
     </iframe>
   );
 }
