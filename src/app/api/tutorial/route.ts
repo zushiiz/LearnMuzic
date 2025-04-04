820313b4f54c3044126692e26855501c51957572
zuu <zxgamingh@gmail.com>
2025-04-04 15:05:55 +0200

    tutorial route api now works, returns response object with songObject and tutorialObject

--- src/app/api/tutorial/route.ts
+++ src/app/api/tutorial/route.ts
@@ -1,9 +1,21 @@
-import { NextRequest } from "next";
+import { NextRequest, NextResponse } from "next/server";
+import { musicDb } from "../../lib/db";
+import { httpErrors } from "../../lib/httpErrors";
 
 export async function GET(req : NextRequest){
   
   const { searchParams } = new URL(req.url);
-  const a = searchParams.get("tutorialPostId");
-  console.log(a);
+  const tutorialPostId = searchParams.get("tutorialPostId");
+  if (!tutorialPostId) {
+    return httpErrors.badRequest;
+  }
+  const tutorialPost = await musicDb.getTutorialPostIdsById(parseInt(tutorialPostId)); 
+
+  const response = {
+    songInfo : await musicDb.getSongById(tutorialPost.songId),
+    tutorialInfo : await musicDb.getTutorialById(tutorialPost.tutorialId)
+  };
+
+  return NextResponse.json(response, {status : 200});
 
 }
