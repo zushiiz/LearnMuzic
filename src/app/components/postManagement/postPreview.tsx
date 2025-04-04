37b497991c60d4dda6a32f3d1f36e0003df4b7c3
zuu <zxgamingh@gmail.com>
2025-04-04 10:42:07 +0200

    wrapped in Link, added href id for tutorial routing

--- src/app/components/postManagement/postPreview.tsx
+++ src/app/components/postManagement/postPreview.tsx
@@ -1,15 +1,19 @@
+import Link from "next/link"
 import { Tutorial, TutorialCardInformation } from "../../lib/dbTypes"
 
-export default function Card({ songTitle, imagePath, songArtist, releaseYear, videoAuthor, instrument, difficulty } : TutorialCardInformation){
+export default function Card({ id, songTitle, imagePath, songArtist, releaseYear, videoAuthor, instrument, difficulty } : TutorialCardInformation){
   return (
-  <ul className="border">
-    <li>{songTitle}</li>
-    <li>{imagePath}</li>
-    <li>{songArtist}</li>
-    <li>{releaseYear}</li>
-    <li>{videoAuthor}</li>
-    <li>{instrument}</li>
-    <li>{difficulty}</li>
-  </ul>
+    <Link href={`/tutorial/${id}`}>
+      <ul className="border">
+        <li>{songTitle}</li>
+        <li>{imagePath}</li>
+        <li>{songArtist}</li>
+        <li>{releaseYear}</li>
+        <li>{videoAuthor}</li>
+        <li>{instrument}</li>
+        <li>{difficulty}</li>
+      </ul>
+    </Link>
+
   );
 }
