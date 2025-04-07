5eeeb8a36bf37688b795799135064ea27bc2961c
zuu <zxgamingh@gmail.com>
2025-04-07 11:18:15 +0200

    Removed debug console.logs

--- src/app/api/tutorialPost/route.ts
+++ src/app/api/tutorialPost/route.ts
@@ -4,13 +4,9 @@ import { httpErrors } from "../../lib/httpErrors";
 
 export async function GET(){
   try {
-
-    console.log("get rn");
     const informationIds = await musicDb.getAllTutorialPosts();
-    console.log("ids", informationIds);
     const allTutorialPosts = await musicDb.getAllTutorialInformationByIds(informationIds);
-    console.log("allTutorials", allTutorialPosts);
-    
+
     return NextResponse.json(allTutorialPosts, {status : 200});
 
   } catch (err) {
