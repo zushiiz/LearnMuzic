37ec0ef6c2d77dd96dea53c62ce871ed2cb378f8
zuu <zxgamingh@gmail.com>
2025-04-18 21:35:37 +0200

    added removeFromSongList api route

---
+++ src/app/api/removeFromSongList/route.ts
@@ -0,0 +1,22 @@
+import {NextRequest, NextResponse} from "next/server";
+import { musicDb } from "../../../app/lib/db";
+import { cookies } from "next/headers";
+import { httpErrors } from "../../lib/httpErrors";
+import { checkUserCookie } from "../../lib/cookie";
+
+export async function POST(req : NextRequest){
+  try {
+    const postId = await req.json();
+    const checkUser = await checkUserCookie();
+    if (!checkUser) {
+      return httpErrors.unauthorized;
+    }
+    const cookie = await cookies();
+    const userId = cookie.get("userId")?.value;
+    await musicDb.removeSongFromUserSongList(postId, Number(userId));
+    return NextResponse.json({message : "Successfully removed"}, {status : 200});
+      
+    } catch (err){
+      return httpErrors.internalServerError;
+    }
+}
