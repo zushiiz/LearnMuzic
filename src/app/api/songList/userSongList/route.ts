6360febcb64092ad7998d64405aeeb27a4437c64
zuu <zxgamingh@gmail.com>
2025-04-20 20:44:16 +0200

    moved to folder /api/songList

---
+++ src/app/api/songList/userSongList/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { musicDb } from "../../../lib/db";
+import { httpErrors } from "../../../lib/httpErrors";
+
+export async function GET(req : NextRequest){
+  try {
+    const { searchParams } = new URL(req.url);
+    const profileId = searchParams.get("profileId");
+
+    if (!profileId){
+      return httpErrors.badRequest;
+    }
+    const response = await musicDb.getUserSongListById(Number(profileId));
+    return NextResponse.json(response, {status : 200});
+
+  } catch (err){
+    console.log(err);
+    return httpErrors.internalServerError;
+  }
+
+}
